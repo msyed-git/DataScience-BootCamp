83e6ec2bf081f6fab246bfc8ebdcd5cfc289bd76
TESLAMOTORS\msyed <msyed@tesla.com>
2024-01-24 13:55:50 -0800

    push SQL query code. completed.

--- SQL-CaseStudy/SQLTasks Tier 2.sql
+++ SQL-CaseStudy/SQLTasks Tier 2.sql
@@ -34,36 +34,56 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
-
+SELECT * FROM Facilities 
+WHERE membercost > 0;
 
 /* Q2: How many facilities do not charge a fee to members? */
-
+SELECT * FROM Facilities 
+WHERE membercost = 0; -- 4 facilities do not charge a member fee. 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost < (monthlymaintenance * 0.2);
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
-
+SELECT *
+FROM Facilities
+WHERE facid in (1, 5);
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
-
+SELECT
+    name,
+    monthlymaintenance,
+    CASE
+        WHEN monthlymaintenance > 100 THEN 'expensive'
+        ELSE 'cheap'
+    END AS cost_label
+FROM
+    Facilities;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
-
+SELECT surname, firstname 
+FROM Members 
+WHERE memid = (SELECT MAX(memid) FROM Members);
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
-
+SELECT DISTINCT b.memid, f.name, CONCAT_WS(' ', m.firstname, m.surname) AS membername
+FROM Bookings AS b
+LEFT JOIN Facilities AS f ON b.facid = f.facid
+LEFT JOIN Members AS m ON b.memid = m.memid
+ORDER BY membername;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -71,10 +91,36 @@ different costs to members (the listed costs are per half-hour 'slot'), and
 the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
-
+SELECT 
+	f.name, 
+	CONCAT_WS(' ', m.firstname, m.surname) AS membername,
+	CASE
+		WHEN m.firstname = 'GUEST' THEN b.slots * f.guestcost
+		ELSE b.slots * f.membercost
+	END AS cost
+FROM Bookings AS b
+LEFT JOIN Facilities AS f ON b.facid = f.facid
+LEFT JOIN Members AS m ON b.memid = m.memid
+WHERE b.starttime LIKE '2012-09-14%'
+ORDER BY cost DESC;
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
-
+SELECT 
+    f.name, 
+    CONCAT_WS(' ', m.firstname, m.surname) AS membername,
+    CASE
+        WHEN m.firstname = 'GUEST' THEN b.slots * f.guestcost
+        ELSE b.slots * f.membercost
+    END AS cost
+FROM Bookings AS b
+LEFT JOIN Facilities AS f ON b.facid = f.facid
+LEFT JOIN Members AS m ON b.memid = m.memid
+WHERE DATE(b.starttime) IN (
+    SELECT DATE(starttime)
+    FROM Bookings
+    WHERE DATE(starttime) = '2012-09-14'
+)
+ORDER BY cost DESC;
 
 /* PART 2: SQLite
 
@@ -85,12 +131,16 @@ QUESTIONS:
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
+##Q10 = pd.read_sql_query("SELECT name, sum(case when memid = 0 then slots * guestcost else slots * membercost end) as Revenue FROM Bookings LEFT JOIN Facilities using(facid) GROUP BY name HAVING Revenue < 1000",engine)
 
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
+##pd.read_sql_query("SELECT m.membername, mm.firstname || ' ' || mm.surname AS Recommended_BY FROM (SELECT firstname || ' ' || surname AS membername, recommendedby FROM Members WHERE recommendedby > 0 ORDER BY surname ASC, firstname ASC) AS m INNER JOIN Members as mm ON m.recommendedby = mm.memid ORDER BY mm.surname ASC, mm.firstname ASC", engine)
 
 
 /* Q12: Find the facilities with their usage by member, but not guests */
-
+##Q12 = pd.read_sql_query("SELECT f.name, m.firstname || ' ' || m.surname AS membername,   COUNT(*) FROM Bookings AS b INNER JOIN Facilities AS f USING(facid) INNER JOIN members AS m USING(memid) WHERE b.memid != 0 GROUP by f.name, membername ",engine)
 
 /* Q13: Find the facilities usage by month, but not guests */
+##Q13 = pd.read_sql_query("SELECT f.name, strftime('%m', starttime) as month,   COUNT(*) AS monthlyusage FROM Bookings AS b INNER JOIN Facilities AS f USING(facid) WHERE b.memid != 0 GROUP by month, f.name ",engine)
+
 
